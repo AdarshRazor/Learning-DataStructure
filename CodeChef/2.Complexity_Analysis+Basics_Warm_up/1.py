4d6943888da96ce846e25ce99fa222b7110506a4
Adarsh Anand <adarshanshu7@gmail.com>
2021-08-02 10:04:42 +0530

    Create 1.py

---
+++ CodeChef/2.Complexity_Analysis+Basics_Warm_up/1.py
@@ -0,0 +1,13 @@
+def main():
+
+    while True:
+        _input = int(input())
+
+        if _input == 42:
+            break
+        else:
+            print(_input)
+
+
+if __name__ == '__main__':
+    main()
