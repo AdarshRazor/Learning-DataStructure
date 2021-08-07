e8643275cd39aaafcbd5811b811ef8488ddcc6ea
Adarsh Anand <adarshanshu7@gmail.com>
2021-08-07 20:08:24 +0530

    Create 3.py

---
+++ CodeChef/2.Complexity_Analysis+Basics_Warm_up/3.py
@@ -0,0 +1,16 @@
+N=int(input())
+
+for i in range(N):
+    wor=input()
+    wl=len(wor)
+    #num=wl//2
+
+    if wl%2==0:
+        a,b=wor[:wl//2],wor[wl//2:]
+    else:
+        a,b=wor[:wl//2],wor[(wl//2)+1:]
+    
+    if sorted(a)==sorted(b):
+        print('YES')
+    else:
+        print('NO')
